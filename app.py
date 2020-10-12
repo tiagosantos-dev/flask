8e1066af555c00987bc56645e33485067777f3d6
Tiago Santos <tiagosantosdev@hotmail.com>
2020-10-12 07:36:29 -0300

    Conhecendo o SQLAlchemy

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from resource.hotel import Hoteis, Hotel
+from sql_alchemy import banco
 
 #pip freeze -> Para saber quais pacotes instalados
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///banco.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
+@app.before_first_request
+def criar_banco():
+    banco.create_all()
 
 api.add_resource(Hoteis, "/hoteis")
 api.add_resource(Hotel, "/hoteis/<int:hotel_id>")
 
 if __name__ == '__main__':
+    #inicializa com o banco de dados
+    banco.init_app(app)
     app.run(debug=True)
 
